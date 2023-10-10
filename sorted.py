297a78da68de4c6a486e4c1cc5408bbe8dcff6c8
Novel-Alam <106124586+Novel-Alam@users.noreply.github.com>
2023-10-09 18:40:01 -0700

    Create sorted.py

---
+++ sorted.py
@@ -0,0 +1,7 @@
+# sorted.py
+
+def reverse_sort_dictionary(input_dict):
+    result_list = []
+    for name, (phone, _) in sorted(input_dict.items(), key=lambda x: x[0], reverse=True):
+        result_list.append((name, phone))
+    return result_list
